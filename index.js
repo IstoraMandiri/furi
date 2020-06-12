9bc0ca483199e7bcc7d7b499041c11faa6a150a9
Chris Hitchcott <hitchcott@gmail.com>
2020-06-12 11:08:58 +0100

    Add definitions

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const Kuroshiro = require('kuroshiro');
-const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji');
+#!/usr/bin/env node
+
+const Kuroshiro = require("kuroshiro");
+const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+var Dictionary = require("japaneasy");
+var dict = new Dictionary({
+  dictionary: "glossing",
+  noRepeat: true,
+});
 
 const charOpts = {
-  to: 'hiragana',
-  mode: 'okurigana',
-  delimiter_start: '[',
-  delimiter_end: ']'
+  to: "hiragana",
+  mode: "okurigana",
+  delimiter_start: "[",
+  delimiter_end: "]",
+};
+
+// disable console.log
+console.log = () => {};
+
+const log = (text) => {
+  process.stdout.write(`${text}\n`);
 };
 
 (async () => {
-  const text = process.argv[2];
-  if (!text) { return console.log("No text specificed"); }
+  const [_a, _b, ..._text] = process.argv;
+  const text = _text.join(" ");
+  if (text.length < 1) {
+    return log("No text specificed");
+  }
   const kuroshiro = new Kuroshiro();
   await kuroshiro.init(new KuromojiAnalyzer());
   const converted = await kuroshiro.convert(text, charOpts);
   // spacing fixing up for anki
   const formatted = converted
-    .split('')
+    .split("")
     .map((char, i) => {
       // if the last previous is a kana, add a space
       if (
@@ -28,8 +45,16 @@ const charOpts = {
       }
       return char;
     })
-    .join('')
-    .replace(/\n /g, '\n');
+    .join("")
+    .replace(/\n /g, "\n");
+
+  const definitions = await dict(text);
+  const formattedDefinitions = definitions
+    .map(
+      (d) =>
+        `${d.japanese}${(d.pronunciation && `[${d.pronunciation}]`) || ""} ${d.english.join(" ")}`
+    )
+    .join("\n");
   // print it
-  return console.log(`---\n\n${formatted}\n\n---\n`);
-})()
+  return log(`---\n\n${formatted}\n\n${formattedDefinitions}\n\n---`);
+})();
